0c36a8b8786a864e2e555f2aaa13be874d1a4f0e
Kooreis <kooreis.official@gmail.com>
2022-02-10 02:00:00 -0600

    Initial setup of the program. It includes the main loop and the menu display.

--- Binary_to_Decimal_Biway_Converter.cs
+++ Binary_to_Decimal_Biway_Converter.cs
@@ -1,4 +1,3 @@
-```CSharp
 using System;
 
 class Program
@@ -11,31 +10,6 @@ class Program
             Console.WriteLine("2. Decimal to Binary");
             Console.WriteLine("3. Exit");
             Console.Write("Enter your choice: ");
-            int choice = Convert.ToInt32(Console.ReadLine());
-
-            switch (choice)
-            {
-                case 1:
-                    Console.Write("Enter a binary number: ");
-                    string binary = Console.ReadLine();
-                    Console.WriteLine("Decimal: " + Convert.ToInt32(binary, 2));
-                    break;
-
-                case 2:
-                    Console.Write("Enter a decimal number: ");
-                    int decimalNumber = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("Binary: " + Convert.ToString(decimalNumber, 2));
-                    break;
-
-                case 3:
-                    Environment.Exit(0);
-                    break;
-
-                default:
-                    Console.WriteLine("Invalid choice");
-                    break;
-            }
         }
     }
 }
-```
