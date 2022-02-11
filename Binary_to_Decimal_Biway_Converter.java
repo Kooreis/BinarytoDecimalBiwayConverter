d576739a32f2ec47e4845f73153c026de839c26b
Kooreis <kooreis.official@gmail.com>
2022-02-11 02:00:00 -0600

    Added a while loop to continuously prompt the user for their choice. The user can choose to convert binary to decimal, decimal to binary, or exit the program.

--- Binary_to_Decimal_Biway_Converter.java
+++ Binary_to_Decimal_Biway_Converter.java
@@ -3,5 +3,12 @@ import java.util.Scanner;
 public class BinaryDecimalConverter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("1. Binary to Decimal");
+            System.out.println("2. Decimal to Binary");
+            System.out.println("3. Exit");
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+        }
     }
 }
