96ef6294a8a2b0b25bf7143f6187bb631b0a5c2e
Kooreis <kooreis.official@gmail.com>
2022-02-11 02:00:00 -0600

    Added user input for menu selection.

--- Binary_to_Decimal_Biway_Converter.cs
+++ Binary_to_Decimal_Biway_Converter.cs
@@ -1,15 +1,1 @@
-using System;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        while (true)
-        {
-            Console.WriteLine("1. Binary to Decimal");
-            Console.WriteLine("2. Decimal to Binary");
-            Console.WriteLine("3. Exit");
-            Console.Write("Enter your choice: ");
-        }
-    }
-}
+int choice = Convert.ToInt32(Console.ReadLine());
